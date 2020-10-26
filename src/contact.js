2f064089bee821a9910d77400c412a7cd2ff79a6
Lilith <themrblacksapce@hotmail.com>
2020-10-25 17:26:25 -0700

    Style contact

--- src/contact.js
+++ src/contact.js
@@ -4,6 +4,7 @@ const contact = (target) => {
   const section = newElement('section', null, 'contact', 'rm');
   
   const sectionChilds = {
+    backGImage: newElement('img', null, 'background', null, './resources/contactbg.jpg'),
     title: newElement('h1', 'Contact us'),
     number: newElement('p', 'Phone: (123)-123-1234', 'contact'),
     email: newElement('p', 'Email: littlemamas@sample.com', 'contact'),
